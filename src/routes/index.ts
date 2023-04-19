c0bd7b61cc26781e5710048762c050d5f8b8e0e0
slayerch1 <cesarjmorais339@gmail.com>
2023-04-18 22:31:56 -0300

    adicionando algumas algumas bibliotecas  e melhorando codigo

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import {StatusCodes} from 'http-status-codes';
+const router = Router();
+
+router.get("/", (req, res) => {
+  return res.send("olá dev");
+});
+
+router.put("/teste", (req, res) => {
+  console.log(req);
+  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+});
+
+export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
-import express from 'express'
-const port = 3000;
+import express from "express";
+import { router } from "./routes";
+import 'dotenv/config'
+
 const app = express();
+app.use(router);
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-  return res.send('olá dev')
-})
-
-
-app.listen(port,()=>{
-    console.log(`back-end rodando na porta: ${port}`)
-})
+app.listen(process.env.PORT || 3000 , () => {
+  console.log(`back-end rodando na porta: ${process.env.PORT}`);
+});
